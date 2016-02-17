98bb34e00cdadec9c95bec27ab80f51536aa0a6f
Matthew Kime <mattk@meetup.com>
2016-02-17 09:37:53 -0500

    don't source, causing problems

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,10 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 cd "$(dirname "${BASH_SOURCE}")"
 git pull origin master
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
 		--exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~
-	source ~/.zshrc
+	#source ~/.zshrc - return to this
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt
